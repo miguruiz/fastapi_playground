3fe40b9259917f7c79f52a73188b659ce31000c5
Miguel Ruiz <miguel.ruiznogues@klm.com>
2025-06-13 13:15:54 +0200

    [TodoApp] Adding get todos by todo_id with code handling

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,10 +1,11 @@
 from typing import Annotated
 
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, Path, HTTPException
 import models
 from models import Todos
 from database import engine, SessionLocal
 from sqlalchemy.orm import Session
+from starlette import status
 
 app = FastAPI()
 
@@ -25,3 +26,12 @@ db_dependency = Annotated[Session, Depends(get_db)]  # This is a type annotation
 @app.get("/")
 async def read_all(db: db_dependency):
     return db.query(Todos).all()  # This will return all the todos from the database
+
+
+@app.get("/todos/{todo_id}", status_code=status.HTTP_200_OK)
+async def read_todo_from_id(db: db_dependency, todo_id: int = Path(gt=0)):
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+    if todo_model is not None:
+        return todo_model
+    else:
+        raise HTTPException(status_code=404, detail='Todo not found.')

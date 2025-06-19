273765c2787f94e75caf7086fac73e0d60263eb1
Miguel Ruiz <miguel.ruiznogues@klm.com>
2025-06-19 23:02:49 +0200

    [TodoApp] Added endpoint to Authenticate by verifying in a user and password match a username in the database and a hashed password

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -8,6 +8,7 @@ from starlette import status
 from database import get_db
 from models import Users
 from passlib.context import CryptContext
+from fastapi.security import OAuth2PasswordRequestForm
 
 router = APIRouter()# If we used FastAPI, it would be an instance of FastAPI, but here we use APIRouter to
 # create a router for the auth endpoints. This allows us to group related endpoints together
@@ -15,6 +16,16 @@ router = APIRouter()# If we used FastAPI, it would be an instance of FastAPI, bu
 bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
 db_dependency = Annotated[Session, Depends(get_db)]  # This is a type annotation for the database dependency
+
+def authenticate_user(username: str, passw: str, db: db_dependency ):
+    user: Users = db.query(Users).filter(Users.username == username).first()
+
+
+    if not user:
+        return False
+    if not bcrypt_context.verify(passw, user.hashed_password):
+        return False
+    return True
 
 class CreateUserRequest(BaseModel):
     username: str
@@ -39,3 +50,13 @@ async def create_user(create_user_request: CreateUserRequest, db: db_dependency)
     db.add(create_user_model)
     db.commit()
     return create_user_model
+
+
+@router.post("/token")
+async def login_for_access_token(from_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency):
+    if not authenticate_user(from_data.username, from_data.password, db):
+        return 'Failed Authentication'
+    else:
+        return 'Authenticated'
+
+    return {"access_token": "token", "token_type": "bearer"}

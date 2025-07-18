de800188a8699f99d98841fb58e5c3d984dfb40e
Miguel Ruiz <miguel.ruiznogues@klm.com>
2025-07-18 22:46:24 +0200

    [TodoApp] Adding an admin route that checks the user role is admin

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -3,12 +3,18 @@ from typing import Annotated, Optional
 from fastapi import FastAPI
 import models
 from database import engine
-from routers import auth, todos
-
+from routers import auth, todos, admin
 
 app = FastAPI()
 
-models.Base.metadata.create_all(bind=engine) # Create the database tables if they do not exist
+models.Base.metadata.create_all(
+    bind=engine
+)  # Create the database tables if they do not exist
 
-app.include_router(auth.router) # Include the auth router to handle authentication-related endpoints
-app.include_router(todos.router) # Include the todos router to handle todo-related endpoints
+app.include_router(
+    auth.router
+)  # Include the auth router to handle authentication-related endpoints
+app.include_router(
+    todos.router
+)  # Include the todos router to handle todo-related endpoints
+app.include_router(admin.router)
--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -38,8 +38,10 @@ def authenticate_user(username: str, passw: str, db: db_dependency):
     return user
 
 
-def create_access_token(username: str, user_id: int, expires_delta: timedelta):
-    encode = {"sub": username, "id": user_id}
+def create_access_token(
+    username: str, user_id: int, role: str, expires_delta: timedelta
+):
+    encode = {"sub": username, "id": user_id, "role": role}
     expire = datetime.now(timezone.utc) + expires_delta
     encode.update({"exp": expire})
     return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
@@ -64,12 +66,13 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
         payload = jwt.decode(token, SECRET_KEY, ALGORITHM)
         username: str = payload.get("sub")
         user_id: int = payload.get("id")
+        user_role: str = payload.get("role")
         if username is None or user_id is None:
             raise HTTPException(
                 status_code=status.HTTP_401_UNAUTHORIZED,
                 detail="Could not validate user.",
             )
-        return {"username": username, "id": user_id}
+        return {"username": username, "id": user_id, "user_role": user_role}
 
     except JWTError:
         raise HTTPException(
@@ -102,5 +105,7 @@ async def login_for_access_token(
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate user."
         )
-    token = create_access_token(user.username, user.id, timedelta(minutes=20))
+    token = create_access_token(
+        user.username, user.id, user.role, timedelta(minutes=20)
+    )
     return {"access_token": token, "token_type": "bearer"}

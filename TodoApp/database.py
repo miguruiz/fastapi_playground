f9b572d37f3913c7f174dbde15a3bce6f0761349
Miguel Ruiz <miguel.ruiznogues@klm.com>
2025-08-22 19:10:09 +0200

    [TodoApp] Modified database to use Postgresql instead of SQLite. Required installing and creating the tables in Postgresql manually.

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -1,25 +1,25 @@
-SQLALCHEMY_DATABASE_URL = "sqlite:///./todoapp.db"
+SQLALCHEMY_DATABASE_URL = (
+    "postgresql://postgres:12345@localhost/TodoApplicationDatabase"
+)
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}) # To enable multiple threads to
+engine = create_engine(SQLALCHEMY_DATABASE_URL)  # To enable multiple threads to
 # access the SQLite database simultaneously.
 
-SessionLocal = sessionmaker (autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
-
 
 
 def get_db():
     db = SessionLocal()
     try:
-        yield db # This is a generator function that yields a database session because FastAPI uses dependency injection
+        yield db  # This is a generator function that yields a database session because FastAPI uses dependency injection
         # to manage resources like database connections. When the request is done, FastAPI will automatically close the session.
         # If we don't use yield here, the session would be closed immediately after the function returns, which would not allow us
         # to use the session in the request handler.
     finally:
         db.close()
-

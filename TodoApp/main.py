fde4165c6d3ee4c1c41ad380cd53dcc94542b63c
miguel <mrnogues89@gmail.com>
2025-09-01 13:30:13 +0200

    [TodoApp] FullStack... Added html & css as well as relevant fastapi imports to render the css and html, and added a test endpoint.

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,11 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 from .database import Base, engine
 from .routers import auth, todos, admin, users
-
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)  # Create the database tables if they do not exist
+
+templates = Jinja2Templates(directory="TodoApp/templates")
+
+app.mount("/static", StaticFiles(directory="TodoApp/static"),name="static")
 
 app.include_router(
     auth.router
@@ -15,6 +20,10 @@ app.include_router(
 )  # Include the todos router to handle todo-related endpoints
 app.include_router(admin.router)
 app.include_router(users.router)
+
+@app.get("/")
+def test(request:Request):
+    return templates.TemplateResponse("home.html",{"request":request})
 
 
 @app.get("/healthy")

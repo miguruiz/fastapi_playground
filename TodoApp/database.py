a05ff4bdd84ae5e664b24c308b03898500ba6910
miguel <mrnogues89@gmail.com>
2025-09-01 13:14:13 +0200

    [TodoApp] Changed postgres use in favor of sqlite

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -1,7 +1,4 @@
-SQLALCHEMY_DATABASE_URL = (
-    "postgresql://postgres:12345@localhost/TodoApplicationDatabase"
-)
-
+SQLALCHEMY_DATABASE_URL = "sqlite:///./todo_application.db"
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
